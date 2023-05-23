d4b671cba44606360672bb1dd80d5a9da48634a8
Emil Motrøen <e.motroen@gmail.com>
2023-05-23 13:26:44 +0200

    Add stacks of four layers

--- Assets/Scripts/AddAlpides.cs
+++ Assets/Scripts/AddAlpides.cs
@@ -7,93 +7,12 @@ using UnityEngine;
 /// </summary>
 public class AddAlpides : MonoBehaviour
 {
-	//[SerializeField]
-	//private GameObject Alpide;
-	
-	//private readonly int _numberOfAlpideLayers = 4;
-	//private float _distanceBetweenAlpideLayers = 0.1f;
+	[SerializeField]
+	private GameObject Alpide;
 
-	//private float _distanceBetweenColliderAndAlpide = 5.0f;
-	//private readonly int _numberOfAlpidesInLayer = 30;
-	//private readonly int _additionalAlpidesToTheSides = 1;  // Add ALPIDEs to the left and right
-	//private readonly float _distanceBetweenAlpidesSides = 1.0f;
-
-	//private Vector3 positionOfCenterOfCollider;
-	//private readonly List<(GameObject, Vector3, Quaternion)> _alpidePositions = 
-	//	new List<(GameObject, Vector3, Quaternion)>();
-
-	//void Start()
-	//{
-	//	positionOfCenterOfCollider = Vector3.zero;
-	//	Spawn();
-	//}
-
-	//public void Spawn()
-	//{
-	//	for (int alpideNumber = 0; alpideNumber < _numberOfAlpidesInLayer; alpideNumber++)
-	//	{
-	//		Vector3 pos = AlpideCircle(positionOfCenterOfCollider, _distanceBetweenColliderAndAlpide);
-	//		Quaternion rot = Quaternion.FromToRotation(Vector3.forward, positionOfCenterOfCollider - pos);
-	//		var alpide = Instantiate(Alpide, pos, rot);
-	//		alpide.name = $"alpide_{alpideNumber}";
-	//		alpide.transform.LookAt(positionOfCenterOfCollider);  // Rotate ALPIDEs to the collision point
-	//		alpide.transform.Rotate(90, 0, 0);  // Add a 90 degree rotation so they face correctly
-	//		_alpidePositions.Add((alpide, pos, rot));
-	//	}
-
-	//	AddAdditionalLayers();
-	//}
-
-	///// <summary>
-	///// Add more layers of ALPIDEs by taking the current ones and adding behind them.
-	///// TODO: Remove distance and use DistanceBetweenAlpideLayers instead.
-	///// </summary>
-	//private void AddAdditionalLayers()
-	//{
-	//	float distance = _distanceBetweenAlpideLayers;
-	//	for (int layerNumber = 1; layerNumber < _numberOfAlpideLayers; layerNumber++)
-	//	{
-	//		foreach (var alpide in _alpidePositions)
-	//		{
-	//			var nextLayerAlpide = Instantiate(Alpide);
-	//			float x = (alpide.Item2.x > 0.0) ?
-	//				alpide.Item2.x + distance : alpide.Item2.x - distance;
-	//			float y = (alpide.Item2.y > 0.0) ?
-	//				alpide.Item2.y + distance : alpide.Item2.y - distance;
-	//			nextLayerAlpide.transform.position = new Vector3(x, y);
-	//			nextLayerAlpide.transform.LookAt(positionOfCenterOfCollider);
-	//			nextLayerAlpide.transform.Rotate(90, 0, 0);
-	//		}
-	//		distance += _distanceBetweenAlpideLayers;
-	//	}
-
-	//	foreach (var alpide in _alpidePositions)
-	//	{
-	//		var nextAlpide = Instantiate(Alpide);
-	//		nextAlpide.transform.position = alpide.Item2;
-	//		nextAlpide.transform.LookAt(positionOfCenterOfCollider);
-	//		nextAlpide.transform.Rotate(90, 0, 0);
-	//	}
-
-	//	_alpidePositions.Clear();
-	//}
-
-	///// <summary>
-	///// Position ALPIDEs around the collision point.
-	///// TODO: Position ALPIDEs systematically instead of randomly
-	///// </summary>
-	///// <param name="center">Center of the collision point.</param>
-	///// <param name="radius">Radius to place ALPIDEs.</param>
-	///// <returns>The position to place ALPIDEs.</returns>
-	//Vector3 AlpideCircle(Vector3 center, float radius)
-	//{
-	//	float angle = Random.value * 360;
-	//	Vector3 pos;
-	//	pos.x = center.x + radius * Mathf.Sin(angle * Mathf.Deg2Rad);
-	//	pos.y = center.y + radius * Mathf.Cos(angle * Mathf.Deg2Rad);
-	//	pos.z = center.z;
-
-	//	return pos;
-	//}
+	void Start()
+	{
+		Debug.Log("AAA");
+	}
 }
 
