76f8b2f3cd8470f00cac1564bf038c6cf814133a
Emil Motrøen <e.motroen@gmail.com>
2023-05-23 13:58:26 +0200

    Add more alpides in code

--- Assets/Scripts/AddAlpides.cs
+++ Assets/Scripts/AddAlpides.cs
@@ -1,18 +1,27 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 /// <summary>
-/// Add the ALPIDEs around a collsion point so they get hit by particles.
+/// Add the ALPIDEs around a collision point so they get hit by particles.
 /// </summary>
 public class AddAlpides : MonoBehaviour
 {
 	[SerializeField]
-	private GameObject Alpide;
+	private GameObject _alpide;
+	private Vector3 center = Vector3.zero;
+
+	private const int _numberOfAlpidesToTheSide = 5;
+	private const float _distanceBetweenAlpides = 1.5f;
 
 	void Start()
 	{
-		Debug.Log("AAA");
+		center = transform.position;
+		for (int i = 0; i <  _numberOfAlpidesToTheSide; i++)
+		{
+			float distance = _distanceBetweenAlpides;
+			center.z += distance;
+			var alpide = Instantiate(_alpide, center, Quaternion.identity);
+			alpide.transform.Rotate(0, 90, 90);
+		}
 	}
 }
 
