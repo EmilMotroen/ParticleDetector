aff79734dfb4f35e7935d4674d19c4dbe59b159c
Emil Motrøen <e.motroen@gmail.com>
2023-05-23 14:38:08 +0200

    Properly add more alpides

--- Assets/Scripts/AddAlpides.cs
+++ Assets/Scripts/AddAlpides.cs
@@ -9,18 +9,27 @@ public class AddAlpides : MonoBehaviour
 	private GameObject _alpide;
 	private Vector3 center = Vector3.zero;
 
-	private const int _numberOfAlpidesToTheSide = 5;
+	private const int _numberOfAlpidesToTheSide = 23;
 	private const float _distanceBetweenAlpides = 1.5f;
 
 	void Start()
 	{
-		center = transform.position;
-		for (int i = 0; i <  _numberOfAlpidesToTheSide; i++)
+		AddAdditionalAlpides();
+	}
+
+	/// <summary>
+	/// Adds more stacks next to the original circle of stacks
+	/// </summary>
+	private void AddAdditionalAlpides()
+	{
+		center = _alpide.transform.position;
+		Debug.Log($"Center: {center}");
+		for (int i = 0; i < _numberOfAlpidesToTheSide; i++)
 		{
 			float distance = _distanceBetweenAlpides;
 			center.z += distance;
 			var alpide = Instantiate(_alpide, center, Quaternion.identity);
-			alpide.transform.Rotate(0, 90, 90);
+			alpide.transform.Rotate(0, 90, 90); // TODO: Rotate in Blender so we don't need to do it in code
 		}
 	}
 }
