4f891f278655d827bd7ba9e457d8f6683db5ac20
Emil Motrøen <e.motroen@gmail.com>
2023-05-23 15:08:55 +0200

    Add TrailColor.cs

---
+++ Assets/Scripts/TrailColor.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Handle the trails colors
+/// </summary>
+public enum TrailColor
+{
+    Red,
+    Green,
+    Blue,
+    Yellow,
+    Magenta
+}
